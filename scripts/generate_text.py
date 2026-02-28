093ff5dbc2a2f64ea924d9585c88d441e5908a91
Wayrion <82315185+Wayrion@users.noreply.github.com>
2026-02-28 14:19:09 +0000

    feat: docker compose

--- scripts/generate_text.py
+++ scripts/generate_text.py
@@ -1,6 +1,7 @@
-def generate_file(size_mb, filename="sample.txt"):
-    target_size = size_mb * 1024 * 1024
+SIZE = 1024
+def generate_file(size_mb, filename=f"sample-{SIZE}KB.txt"):
+    target_size = size_mb * 1024
     with open(filename, "wb") as f:
         f.write(b"A" * target_size)
 
-generate_file(1024)
+generate_file(SIZE)
